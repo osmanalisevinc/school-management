04ff292b42da443f538955989b290cc12941028f
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-09 01:35:47 +0300

    added footer css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
-
   return (
-    <div>
-        <div>@2025 Mamamia. All rights reserved.</div>
-        <div></div>
+    <div className={styles.container}>
+      <div>@2025 Mamamia. All rights reserved.</div>
+      <div>
+        <div className={styles.social}>
+          <Image  className={styles.icon} src="/face.png" alt="Lama dev"   width={15} height={15} />
+          <Image  className={styles.icon} src="/insta.jpg" alt="Lama dev"   width={15} height={15} />
+          <Image  className={styles.icon} src="/twitter.png" alt="Lama dev"  width={15} height={15} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
