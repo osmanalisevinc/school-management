5e939cc3ebe8725db3b4e3d66d3fb9e0130a54ad
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-11 20:22:14 +0300

    added home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import schoolImage from "../../public/school.jpg";
 
 export default function Home() {
   return (
-    <div>Home </div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products</h1>
+        <p className={styles.desc}>Turning your idea into reality</p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={schoolImage} alt="" className={styles.image} />
+      </div>
+    </div>
   );
 }
