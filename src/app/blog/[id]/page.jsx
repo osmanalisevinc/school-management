7e62e384c00a985f188e6cebb70b8b95d4f129ad
osmanalisevinc <Sevinc32isparta@gmail.com>
2025-07-12 15:05:57 +0300

    blog added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
 const BlogPost = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
+          <h1 className={styles.title}>Title</h1>
           <p className={styles.desc}>
-            {data.desc}
+            Desc
           </p>
           <div className={styles.author}>
             <Image
-              src={data.img}
+              src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg"
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>{data.username}</span>
+            <span className={styles.username}>osman doe</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src={data.img}
+            src="https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg"
             alt=""
             fill={true}
             className={styles.image}
@@ -31,7 +32,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-         {data.content}
+        content
         </p>
       </div>
     </div>
---
+++ src/components/darkmodetoggle/DarkModeToggle.jsx
@@ -0,0 +1,21 @@
+
+"use client"
+import React, { use, useContext } from "react";
+import styles from "./darkmode.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
+
+function DarkModeToggle() {
+  const { toggle,mode } = useContext(ThemeContext);
+  return (
+    <div className={styles.container} onClick={toggle}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🔆</div>
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
+    </div>
+  );
+}
+
+export default DarkModeToggle;
